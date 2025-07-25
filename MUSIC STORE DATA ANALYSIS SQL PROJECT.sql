44905f6495aef8f9ea7a548fb9729c1f8765ee04
wwwadity <wwwaditya.simgh@gmail.com>
2025-07-25 15:43:40 +0530

    Update MUSIC STORE DATA ANALYSIS SQL PROJECT.sql

--- MUSIC STORE DATA ANALYSIS SQL PROJECT.sql
+++ MUSIC STORE DATA ANALYSIS SQL PROJECT.sql
@@ -81,39 +81,7 @@ ORDER BY
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* Write a query that determines the customer that has spent the most on music for each 
+/*6 Write a query that determines the customer that has spent the most on music for each 
 country. Write a query that returns the country along with the top customer and how 
 much they spent. For countries where the top amount spent is shared, provide all 
 customers who spent this amount */
